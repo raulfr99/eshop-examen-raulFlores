b9e39e4e33b423f117c25b1765812b173be9ef0e
Raul Ernesto Flores Ramirez <raulfr_99@hotmail.com>
2020-12-04 21:48:54 -0600

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      items:[]
+    }
+  } 
+  componentDidMount(){
+    var url = 'https://eshop-deve.herokuapp.com/api/v2/orders';
+    fetch(url, {
+      method: 'POST', // or 'PUT' // data can be `string` or {object}!
+      headers:{
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYwNTY0NDA0NzA1OH0.skfIY_7CAANkxmhoq37OI4jYRE8flx1ENq1v1VaRevJiroYNFQYz7Oy6hL1YZ1OJkevXSQFuLMHTqY0w6d5nPQ"
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+  }
+  
+  render(){
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        hola
+      </h1>
     </div>
   );
+}
 }
 
 export default App;
